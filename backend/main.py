df9359880141e356a0603e620fc55bd8f775e68c
ranin26 <airrundwoolen126@gmail.com>
2022-10-09 13:03:37 -0400

    updates

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,6 @@ from sqlite3 import Error
 
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
-    conn = None
     try:
         conn = sqlite3.connect(db_file)
         print(sqlite3.version)
@@ -37,31 +36,137 @@ def create_connection(db_file):
 
 app = FastAPI()
 
-conn = None
-
 @app.on_event("startup")
 async def startup_event():
     sql_create_depots_table = """CREATE TABLE IF NOT EXISTS depots (
                                         id integer PRIMARY KEY,
                                         name text NOT NULL,
                                         distance float,
-                                        travel_time integer NOT NULL,
-                                        FOREIGN KEY (project_id) REFERENCES projects (id)
+                                        travel_time integer NOT NULL
                                     );"""
 
     sql_create_queues_table = """CREATE TABLE IF NOT EXISTS queues (
                                         id integer PRIMARY KEY,
                                         queue_time integer NOT NULL,
-                                        FOREIGN KEY (project_id) REFERENCES depots (id)
+                                        depot_id integer NOT NULL,
+                                        FOREIGN KEY (depot_id) REFERENCES depots (id)
                                     );"""
 
+    global conn
     conn = create_connection("sqlite\db\pythonsqlite.db")
 
     if conn is not None:
+        delete_all_tables(conn)
         create_table(conn, sql_create_depots_table)
         create_table(conn, sql_create_queues_table)
+        add_data(conn)
     else:
         print("Error! cannot create the database connection.")
+
+def delete_all_tables(conn):
+    """
+    Delete all tables
+    :param conn: Connection to the SQLite database
+    :return:
+    """
+    sql = 'DROP TABLE IF EXISTS depots'
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+
+    sql = 'DROP TABLE IF EXISTS queues'
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+
+@app.on_event("shutdown")
+def shutdown_event():
+    if(conn is not None):
+        conn.close()
+
+
+def add_data(conn):
+    """
+    Create a new depot
+    :param conn:
+    :param task:
+    :return:
+    """
+
+    depot = ('Drop off 1', 7, 15)
+
+    sql = ''' INSERT INTO depots(name, distance, travel_time)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, depot)
+    conn.commit()
+    depot_id = cur.lastrowid
+
+    queue = (7, depot_id)
+
+    sql = ''' INSERT INTO queues(queue_time, depot_id)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, queue)
+    conn.commit()
+
+    queue = (12, depot_id)
+
+    sql = ''' INSERT INTO queues(queue_time, depot_id)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, queue)
+    conn.commit()
+
+    depot = ('Drop off 2', 9, 23)
+
+    sql = ''' INSERT INTO depots(name,distance, travel_time)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, depot)
+    conn.commit()
+    depot_id = cur.lastrowid
+
+    queue = (10, depot_id)
+
+    sql = ''' INSERT INTO queues(queue_time, depot_id)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, queue)
+    conn.commit()
+
+    queue = (16, depot_id)
+
+    sql = ''' INSERT INTO queues(queue_time, depot_id)
+                  VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, queue)
+    conn.commit()
+
+    depot = ('Drop off 3', 11, 30)
+
+    sql = ''' INSERT INTO depots(name,distance, travel_time)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, depot)
+    conn.commit()
+    depot_id = cur.lastrowid
+
+    queue = (5, depot_id)
+
+    sql = ''' INSERT INTO queues(queue_time, depot_id)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, queue)
+    conn.commit()
+
+    queue = (8, depot_id)
+
+    sql = ''' INSERT INTO queues(queue_time, depot_id)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, queue)
+    conn.commit()
 
 
 def create_table(conn, create_table_sql):
@@ -95,7 +200,36 @@ class Depo(BaseModel):
     queues:tuple
 
 
-queue = Queue(id=0, queue_time=5)
+def select_all_depots(conn):
+    """
+    Query all rows in the depots table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM depots")
+
+    rows = cur.fetchall()
+
+    depos = []
+
+    # for row in rows:
+    #     cur = conn.cursor()
+    #     cur.execute(f"SELECT * FROM queues where depot_id = '{row[0]}'")
+    #
+    #     ques = ()
+    #     rows = cur.fetchall()
+    #     for row in rows:
+    #         ques(Queue(id=row[0], queue_time=row[1]))
+    #     depos.append(Depo(
+    #     id=row[0],
+    #     name=row[1],
+    #     distance=row[2],
+    #     travel_time=row[3],
+    #     queue=ques))
+
+    return depos
+
 
 depos = [Depo(
         id=0,
@@ -114,14 +248,7 @@ depos = [Depo(
 
 @app.get("/depos")
 async def get_depos():
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM depots")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-    conn.close()
+    depots = select_all_depots(conn)
     return depos
 
 
